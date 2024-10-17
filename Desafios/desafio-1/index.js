0779a9023a5cadfdf1208442b3c398fd46f2285b
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-10-17 10:51:03 -0300

    atualizado o código do desafio 1 e todos os README.md

--- Desafios/desafio-1/index.js
+++ Desafios/desafio-1/index.js
@@ -1,60 +1,23 @@
-/*
-# 1️⃣ Desafio Classificador de nível de Herói
-
-**O Que deve ser utilizado**
-
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-
-## Objetivo
-
-Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-Se XP for menor do que 1.000 = Ferro
-Se XP for entre 1.001 e 2.000 = Bronze
-Se XP for entre 2.001 e 5.000 = Prata
-Se XP for entre 5.001 e 7.000 = Ouro
-Se XP for entre 7.001 e 8.000 = Platina
-Se XP for entre 8.001 e 9.000 = Ascendente
-Se XP for entre 9.001 e 10.000= Imortal
-Se XP for maior ou igual a 10.001 = Radiante
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói de nome **{nome}** está no nível de **{nivel}**"
-*/
-
 const name = 'Dark Lord 666'
-let XP = 5000
+let XP = 666000
 let lvl = ''
 
-switch (XP) {
-    case (XP <= 1000):
-        lvl = 'Ferro';
-        break
-    case (XP > 1000 && XP <= 2000):
-        lvl = 'Bronze';
-        break
-    case (XP > 2000 && XP <= 5000):
-        lvl = 'Prata';
-        break
-    case (XP > 5000 && XP <= 7000):
-        lvl = 'Ouro';
-        break
-    case (XP > 7000 && XP <= 8000):
-        lvl = 'Platina';
-        break
-    case (XP > 8000 && XP <= 9000):
-        lvl = 'Ascendente';
-        break
-    case (XP > 9000 && XP <= 10000):
-        lvl = 'Imortal';
-        break
-    default:
-        lvl = 'Radiante';
+if (XP <= 1000) {
+    lvl = 'Ferro';
+} else if (XP > 1000 && XP <= 2000) {
+    lvl = 'Bronze';
+} else if (XP > 2000 && XP <= 5000) {
+    lvl = 'Prata';
+} else if (XP > 5000 && XP <= 7000) {
+    lvl = 'Ouro';
+} else if (XP > 7000 && XP <= 8000) {
+    lvl = 'Platina';
+} else if (XP > 8000 && XP <= 9000) {
+    lvl = 'Ascendente';
+} else if (XP > 9000 && XP <= 10000) {
+    lvl = 'Imortal';
+} else {
+    lvl = 'Radiante';
 }
 
 console.log(`O herói de nome ${name} está no nível ${lvl}`)
